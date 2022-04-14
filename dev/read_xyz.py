e37e000b1f4ed52e16d639010a0eb02e3cf42bb9
David Russell <davidrussell327@gmail.com>
2022-04-14 19:07:10 -0400

    Working error visualization

---
+++ dev/read_xyz.py
@@ -0,0 +1,55 @@
+import rosbag
+import pandas as pd
+import matplotlib.pyplot as plt
+import pdb
+from numpy_ros import to_numpy, to_message
+from geometry_msgs.msg import Point
+import numpy as np
+import pyvista as pv
+
+
+FILE = "/home/frc-ag-1/dev/courses/KDC_project/docker_project/ws/data/2022-04-14-17-53-24.bag"
+bag = rosbag.Bag(FILE)
+
+gt_poses = []
+goal_poses = []
+errors = []
+
+for topic, msg, t in bag.read_messages():
+    t = t.to_time()
+    if topic == "/uav1/ground_truth/state":
+        gt_poses.append(
+            [
+                msg.pose.pose.position.x,
+                msg.pose.pose.position.y,
+                msg.pose.pose.position.z,
+                t,
+            ]
+        )
+    elif topic == "/uav1/tracking_point":
+        goal_poses.append(
+            [
+                msg.pose.pose.position.x,
+                msg.pose.pose.position.y,
+                msg.pose.pose.position.z,
+                t,
+            ]
+        )
+    elif topic == "/uav1/tracking_error":
+        errors.append([msg.data, t])
+    else:
+        pdb.set_trace()
+
+gt_poses, goal_poses, errors = [
+    np.stack(x, axis=0) for x in (gt_poses, goal_poses, errors)
+]
+
+plotter = pv.Plotter()
+
+gt_pc = pv.PolyData(gt_poses[:, :3])
+goal_pc = pv.PolyData(goal_poses[:, :3])
+plotter.add_mesh(gt_pc, color="r")
+plotter.add_mesh(goal_pc, color="b", scalars=errors[:, 0])
+plotter.show()
+
+pdb.set_trace()
